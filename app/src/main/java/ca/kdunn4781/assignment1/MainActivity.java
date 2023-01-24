53468aa3b429fc5474909bd56a967ca28f8bb8b6
kdunn4781 <kdunn4781@conestogac.on.ca>
2023-01-24 10:44:55 -0500

    Initial Commit

---
+++ app/src/main/java/ca/kdunn4781/assignment1/MainActivity.java
@@ -0,0 +1,21 @@
+package ca.kdunn4781.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import ca.kdunn4781.assignment1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
