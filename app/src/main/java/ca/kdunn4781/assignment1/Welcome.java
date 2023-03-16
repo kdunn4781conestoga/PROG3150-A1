e094acc06d55cf1b5a2df48977af112870c2dccc
Caine Phung <Hphung3324@conestogac.on.ca>
2023-03-15 20:34:22 -0500

    add welcome screen

---
+++ app/src/main/java/ca/kdunn4781/assignment1/Welcome.java
@@ -0,0 +1,24 @@
+package ca.kdunn4781.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import ca.kdunn4781.assignment1.databinding.ActivityMainBinding;
+import ca.kdunn4781.assignment1.location.LocationActivity;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.Calendar;
+public class Welcome {
+}
