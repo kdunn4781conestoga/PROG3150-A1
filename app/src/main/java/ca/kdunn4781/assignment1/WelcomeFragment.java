a9848a89d0217eff56a2967eb0daa54386647c98
Caine Phung <Hphung3324@conestogac.on.ca>
2023-04-13 21:20:42 -0500

    implemented start and stop service

--- app/src/main/java/ca/kdunn4781/assignment1/WelcomeFragment.java
+++ app/src/main/java/ca/kdunn4781/assignment1/WelcomeFragment.java
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,9 +16,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import ca.kdunn4781.assignment1.ServicesAndReceivers.Network;
 import ca.kdunn4781.assignment1.databinding.FragmentWelcomeBinding;
 import ca.kdunn4781.assignment1.trip.NewTripFragment;
 import ca.kdunn4781.assignment1.trip.SavedTripsFragment;
+import android.content.Intent;
 
 
 @RequiresApi(api = Build.VERSION_CODES.N)
@@ -27,6 +30,20 @@ public class WelcomeFragment extends Fragment {
     public WelcomeFragment() {
         // Required empty public constructor
     }
+
+    public void startService(View view) {
+        Intent intent = new Intent(getActivity().getBaseContext(), Network.class);
+        getActivity().startService(intent);
+
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        Intent intent = new Intent(getActivity().getBaseContext(), Network.class);
+        getActivity().stopService(intent);
+
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,6 +60,7 @@ public class WelcomeFragment extends Fragment {
         binding.btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 // if Permission is not granted, ask user for permission
 //                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
 //                        && getActivity().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
