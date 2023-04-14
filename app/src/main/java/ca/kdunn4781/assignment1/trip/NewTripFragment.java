5c3c0f54ee20aeca20d5bc11cfaac170a1cce5e0
Caine Phung <Hphung3324@conestogac.on.ca>
2023-04-14 13:06:05 -0500

    Update NewTripFragment.java

--- app/src/main/java/ca/kdunn4781/assignment1/trip/NewTripFragment.java
+++ app/src/main/java/ca/kdunn4781/assignment1/trip/NewTripFragment.java
@@ -60,8 +60,7 @@ public class NewTripFragment extends Fragment {
     public NewTripFragment() {
         // Required empty public constructor
     }
-
-
+    
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
